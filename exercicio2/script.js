31a67f448be7b0672bd5dfba155c5ccc93924089
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-09-14 15:26:19 -0300

    Aula 17, cada dia mais dificil, sigo no aprendizado diario

---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+//A//
+function calculaDoisNum (n1,n2){
+    console.log(`${n1} + ${n2} = ${n1 + n2}`)
+}
+//B//
+function validaNumero (n1,n2){
+    console.log(`${n1} é maior que ${n2}? ${n1>=n2}`)
+}
+validaNumero(Number(prompt("Informe o primeiro numero")), Number(prompt("Informe o segundo numero")))
